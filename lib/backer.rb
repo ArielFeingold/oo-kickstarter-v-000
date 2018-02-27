fe2bbe06ca073bd9818228672145cf217fda4f98
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 18:40:55 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,9 +12,8 @@ class Backer
   def back_project(title)
       p = Project.new(title)
       @backed_projects << p.title
-      binding.pry
+      # binding.pry
       p.backers << self
-
   end
 
 end
