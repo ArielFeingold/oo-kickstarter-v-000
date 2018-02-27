555c1c1f025bae01c677ec9baa4b5a4e72c92f82
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 18:29:12 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,15 @@
 class Backer
 
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+      p = Project.new
+      @backed_projects << p
   end
 
 end
